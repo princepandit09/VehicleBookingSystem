4ef12a5b8e83e73837ec3f940511c7464639b2ed
Sumittechpro <sumit.yadav@techprocompsoft.com>
2024-02-07 17:39:02 +0530

    Add project files.

---
+++ VehicleBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
